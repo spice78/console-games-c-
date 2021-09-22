149a9127d161ef9b3dca246a858aa80563be75fc
Andre V Blokhin <61274717+spice78@users.noreply.github.com>
2021-09-22 15:29:33 +0300

    Add files via upload
    Try to good manage files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace whack_a_mole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wackamole wam = new Wackamole();
+            wam.Struck();
+            //Console.WriteLine(wam);            
+        }
+    }
+}
