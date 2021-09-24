79d35b4ede08369dbbd7f9a7012638363ca293d6
spice78 <spicerama@gmail.com>
2021-09-24 10:47:40 +0300

    tic tac toe started, pls check in github
    Signed-off-by: spice78 <spicerama@gmail.com>

---
+++ TicTacToe/tictactoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
